1d9211e482518102912973ad282b999f6e4938e2
Carlos Sánchez <cfsancas@upo.es>
2024-08-01 17:07:02 +0200

    added path to database download directories

--- config.py
+++ config.py
@@ -4,4 +4,9 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
  
 BASE_DIR = os.path.dirname(current_dir)
 DATA_DIR = os.path.join(BASE_DIR, 'databases')
+CARD_DIR = os.path.join(DATA_DIR, 'card')
+AMRFINDERPLUS_DIR = os.path.join(DATA_DIR, 'amrfinderplus')
+RESFINDER_DIR = os.path.join(DATA_DIR, 'resfinder')
+POINTFINDER_DIR = os.path.join(DATA_DIR, 'pointfinder')
+
 CODE_DIR = os.path.join(BASE_DIR, 'code')

ceafd497fb404eb15abb2596285b0e409b059e42
cfsancas <cfsancas@upo.es>
2024-08-05 13:30:42 +0200

    add new path to config

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 current_dir = os.path.dirname(os.path.abspath(__file__))
  
 BASE_DIR = os.path.dirname(current_dir)
+MAP_DIR = os.path.join(BASE_DIR, 'map_tsv')
 DATA_DIR = os.path.join(BASE_DIR, 'databases')
 CARD_DIR = os.path.join(DATA_DIR, 'card')
 AMRFINDERPLUS_DIR = os.path.join(DATA_DIR, 'amrfinderplus')

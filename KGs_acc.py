8b6ed641a26b81b4dc53dc10e78f96fd4e423d05
cfsancas <cfsancas@upo.es>
2024-08-05 13:34:29 +0200

    add color to KGs and bug corrections

--- KGs_acc.py
+++ KGs_acc.py
@@ -11,7 +11,7 @@ from common_functions import amrfinderplus_graph
 from common_functions import resfinder_graph
 from common_functions import simplify_graph
 from common_functions import insert_newline_every_n_chars
-from config import BASE_DIR, DATA_DIR, CODE_DIR
+from config import BASE_DIR, DATA_DIR, CARD_DIR, MAP_DIR
 
 parser = argparse.ArgumentParser(description='Create knowledge graph from UniProt KB accession with CARD ontology')
 parser.add_argument('--accession', type=str, help='UniProt KB accession', required=True)
@@ -23,11 +23,17 @@ outdir = args.outdir
 
 ### Define group color mapping
 group_colors = {
-	'card': 'blue',
+	'card': '#0000FF', #blue
 	'uniprot': 'red',
 	'amrfinderplus': 'green',
 	'resfinder': 'orange'
 	# Add more groups and their colors as needed
+}
+card_colors = {
+  'card': '#0000FF', # blue
+  'AMR Gene Family': '#8A2BE2', # violet blue
+  'Drug Class': '#00BFFF', # Depskyblue
+  'Resistance Mechanism': '#B0E0E6' # pale blue
 }
 
 ### create graph
@@ -36,11 +42,12 @@ G = nx.MultiDiGraph()
 
 ### Create CARD KG
 card_G, aro = card_graph(
-	obo_file=DATA_DIR + '/card/card-ontology/aro.obo',
-  json_file=DATA_DIR + '/card/card-data/card.json',
-	card_map_file=BASE_DIR + '/map_tsv/card_map.tsv', 
+	obo_file=CARD_DIR + '/ontology/aro.obo',
+  json_file=CARD_DIR + '/data/card.json',
+  categories_file=CARD_DIR + '/data/aro_categories.tsv',
+	map_file=MAP_DIR + '/card_map.tsv',
 	acc=acc, 
-	color=group_colors.get('card', 'black')
+	colors=card_colors
 	)
 ### Create AMRFinderPlus graph
 amrfinderplus_G, gene_fam = amrfinderplus_graph(
@@ -79,7 +86,7 @@ if resfinder_G is not None:
   G = nx.union(G, resfinder_G)
   G.add_edge(acc, gene_accession, label='is')
 
-### merge nodes with same predecesor and sucesor nodes
+### merge nodes with same predecesor and sucesor nodes !!! on develop
 # G = simplify_graph(graph=G)
 
 # Create a PyVis network
@@ -92,8 +99,9 @@ net.from_nx(G)
 for node in net.nodes:
   # print(node)
   # node['title'] = insert_newline_every_n_chars(node['title'], 80)  # Display label on hover
-  node['label'] = node['label']  # Show label on the node
-  node['color'] = G.nodes[node['id']].get('color', 'black')
+  node['label'] = insert_newline_every_n_chars(node.get('label', ''), 80)  # Show wrap label on the node
+  node['title'] = insert_newline_every_n_chars(node.get('title', ''), 80)  # Show wrap title on the node
+  node['color'] = G.nodes[node['id']].get('color', 'black') # get color from networkx graph node
   node['font'] = {'size': 45, 'color': 'white', 'vadjust': 0, 'multi': 'html'}  # Increase font size and change color to white
   node['size'] = 25  # Increase node size
 

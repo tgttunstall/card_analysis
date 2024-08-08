e99cf472414aac2537dd09de3340e850962d2e3d
cfsancas <cfsancas@upo.es>
2024-08-08 13:29:44 +0200

    update paths to pathlib

--- map_prot_acc.py
+++ map_prot_acc.py
@@ -2,21 +2,21 @@
 # coding: utf-8
 
 import pandas as pd
-from config import BASE_DIR, DATA_DIR, CODE_DIR
+from config import MAP_DIR, CARD_DIR, AMRFINDERPLUS_DIR, RESFINDER_DIR, POINTFINDER_DIR
 from common_functions import mapping_card_protein_accession
 from common_functions import mapping_armfinderplus_protein_accession
 from common_functions import mapping_resfinder_protein_accession
  
 
 def main():
-    df = mapping_card_protein_accession(card_data_dir=DATA_DIR + '/card/card-data/' )
-    df.to_csv(BASE_DIR + '/map_tsv/card_map.tsv', sep='\t', index=False)
+    df = mapping_card_protein_accession(card_data_dir=CARD_DIR + 'data/' )
+    df.to_csv(MAP_DIR / 'card_map.tsv', sep='\t', index=False)
 
-    df = mapping_armfinderplus_protein_accession(amrfinderplus_dir=DATA_DIR + '/AMRFinderPlus/')
-    df.to_csv(BASE_DIR + '/map_tsv/amrfinderplus_map.tsv', sep='\t', index=False)
+    df = mapping_armfinderplus_protein_accession(amrfinderplus_dir=AMRFINDERPLUS_DIR)
+    df.to_csv(MAP_DIR / 'amrfinderplus_map.tsv', sep='\t', index=False)
 
-    df = mapping_resfinder_protein_accession(resfinder_dir=DATA_DIR + '/resfinder_db/')
-    df.to_csv(BASE_DIR + '/map_tsv/resfinder_map.tsv', sep='\t', index=False)
+    df = mapping_resfinder_protein_accession(resfinder_dir=RESFINDER_DIR)
+    df.to_csv(MAP_DIR / 'resfinder_map.tsv', sep='\t', index=False)
 
 if __name__ == "__main__":
     main()
